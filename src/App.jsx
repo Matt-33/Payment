8372345e9cf073fd2802e51c7f9a39652af4eec5
Matt-33 <matthias.giraudeau@gmail.com>
2024-06-06 13:02:55 +0200

    payment front

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import CheckoutForm from './components/checkoutform'
+
+function App() {
+  return(
+    <div className='App'>
+      <header className='App-header'>
+        <h1>V.2</h1>
+        <CheckoutForm/>
+      </header>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/checkoutform.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import { useStripe, useElements, CardElement} from "@stripe/react-stripe-js";
+
+const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const [email , setEmail] = useState('');
+
+const handleSubmit = async(event) =>{
+    event.preventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+        type : 'card',
+        card: elements.getElement(CardElement),
+        billing_details : {
+            email: email, 
+        },
+    });
+    if (!error) { 
+        const response = await fetch('http://localhost:3000/api/payment', {
+            method: 'POST',
+            headers: 
+            {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ paymentMethodId: paymentMethod.id }),
+        });
+        if(response.ok){
+            console.log('Paiement reussie')
+        }else{
+            console.log('Paiement refusé')
+        }
+    }else {
+        console.log(error)
+    }
+};
+return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+        required
+      />
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Payer
+      </button>
+    </form>
+  );
+}
+
+export default CheckoutForm;
